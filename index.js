88ecb24d77082abf3e04018a2c106c0118d83bd2
Mimi Doan <md3543@nyu.edu>
2018-01-06 16:56:46 -0500

    comments add all pieces in place for something

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Twit = require('twit');
 const request = require('request');
 const keys = require('./keys');
 
+
+// create a new TWIT obj to connect to your account
 const TWIT = new Twit({
   consumer_key: keys.API_KEY,
   consumer_secret: keys.SECRET_KEY,
@@ -9,31 +11,35 @@ const TWIT = new Twit({
   access_token_secret: keys.ACCESS_SECRET
 });
 
+/*======== API for testing account connection/posting ===========================================================
+
+======================================================================================================*/
 
 const quoteURL = 'https://api.forismatic.com/api/1.0/?method=getQuote&key=123456&format=text&lang=en';
 
-
+// issue a request to the api to get quote
 const getQuote = new Promise(function(fulfill, reject){
   request(quoteURL, function(err, res, body) {
-    // if(err) {
-    //   reject(err);
-    // } else {
-    //   fulfill(body);
-    // }
-
     err ? reject(err) : fulfill(body);
-
   });
 });
 
+// post the quote to twitter
 function postTweet(tweet) {
   console.log(tweet);
-  TWIT.post('statuses/update', {status: tweet}, function(err, data, res) {
+
+  // user Twit Post strategy
+  // TWIT.post('statuses/update', {status: tweet}, function(err, data, res) {
+  //   console.log('$$$$$$ DATA', data);
+  //   // console.log('$$$$$$ RESPONSE', res);
+  // });
+
+  TWIT.post('statuses/update', {status: 'hello world'}, function(err, data, res) {
     console.log('$$$$$$ DATA', data);
-    console.log('$$$$$$ RESPONSE', res);
+    // console.log('$$$$$$ RESPONSE', res);
   });
 }
 
-
+// call the promise and catch any errors
 getQuote.then(postTweet)
         .catch(console.log)
--- scrape.js
+++ scrape.js
@@ -1,26 +1,23 @@
+//  cheerio lib for web scraping
 const cheerio = require('cheerio');
 const request = require('request');
 
-
+// issue a request to the webpage
 const scrape = new Promise((fulfill, reject) => {
   const url = `https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=${9}&src=hp-gem`;
   request(url, function(err, res, body) {
-    err ? reject(err) : fulfill(body);;
+    err ? reject(err) : fulfill(body);
   });
 });
 
 
 function parseRequest(body) {
   const $ = cheerio.load(body);
-
-  // $('.horoscope-content').filter(function() {
-  //   console.log(this)
-  // })
-  console.log($(`.horoscope-content p`).text());
-  // console.log(body);
+  const load = $(`.horoscope-content p`).text();
+  console.log(load);
 }
 
 
 
 scrape.then(parseRequest)
-     .catch(console.log);
+      .catch(console.log);
